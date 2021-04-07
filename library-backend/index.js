46db0da2dc6494bb14123f89fc636dfe1f46e0cf
Anar Ansarova <Anar_Ansarova@epam.com>
2021-04-07 17:32:10 +0600

    task 8.13

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,20 @@
 const {ApolloServer, gql} = require('apollo-server')
 const {v1: uuid} = require('uuid')
+const mongoose = require('mongoose')
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+const MONGODB_URI = 'mongodb+srv://fullstack:fullstack@cluster0.slx8m.mongodb.net/library?retryWrites=true&w=majority'
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let authors = [
     {
@@ -111,8 +126,8 @@ type Book {
 
 const resolvers = {
     Query: {
-        bookCount: () => books.length,
-        authorCount: () => authors.length,
+        bookCount: () => Book.collection.countDocuments(),
+        authorCount: () => Author.collection.countDocuments(),
         allBooks: (root, args) => {
             if (!args.author && !args.genre) {
                 return books
